884eee7c449d91cf9c45e5fa807538b7e6538be4
Ritvick Lohani <lohaniritvick@gmail.com>
2023-02-07 18:52:35 +0530

    firestoretest init

--- src/main/java/com/javaproject/init.java
+++ src/main/java/com/javaproject/init.java
@@ -1,12 +1,16 @@
+package com.javaproject;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
 
 public class init{
-    void initialiseFirebase() throws IOException {
+    public static void initialiseFirebase() throws IOException {
         FileInputStream serviceAccount = new FileInputStream(
                 "/home/pi/Desktop/javaprojectopenstore/serviceAccountKey.json");
 
