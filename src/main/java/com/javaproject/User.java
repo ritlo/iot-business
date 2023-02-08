d95496a84f02a88dbf371ab63db521bee5fbf7d6
Ritvick Lohani <lohaniritvick@gmail.com>
2023-02-08 18:46:09 +0530

    Update User class

--- src/main/java/com/javaproject/User.java
+++ src/main/java/com/javaproject/User.java
@@ -1,6 +1,8 @@
 package com.javaproject;
 
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
 import com.password4j.Hash;
@@ -9,16 +11,23 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+
+
 public class User implements Serializable{
     public String username;
-    public int authorizationLevel;
+    public boolean admin;
     public Map <String, Integer> DailyCount = new HashMap<String, Integer>();
     public Map <String, ArrayList<String>> PersonCount = new HashMap<String, ArrayList<String>>();
     public String hash;
     
-    User(String username, String hash) {
+    User(String username, String hash, boolean admin) {
         this.username = username;
         this.hash = hash;
+        this.admin = admin;
+        DateTimeFormatter currentDateFormatter = DateTimeFormatter.ofPattern("dd/MM/YYYY");
+        LocalDate currentDate = LocalDate.now();
+        this.DailyCount.put(currentDate.toString(), 0);
+        this.PersonCount.put(currentDate.toString(),null);
     }
 
     void addcount(String date, int count) {
